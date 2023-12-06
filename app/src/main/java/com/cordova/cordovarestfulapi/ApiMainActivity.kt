2109646b3f5ec3e48f481718777d0c94d2e8600a
Cordova_RM <cordovaroselymae@gmail.com>
2023-12-06 15:33:50 +0800

    1st commit

---
+++ app/src/main/java/com/cordova/cordovarestfulapi/ApiMainActivity.kt
@@ -0,0 +1,13 @@
+package com.cordova.cordovarestfulapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApiMainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_api_main)
+    }
+}
